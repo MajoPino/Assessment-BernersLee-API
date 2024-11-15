35abf5bd2c53204ff2c543fa013a42035132f0e5
MajoPino <majo12pino@gmail.com>
2024-11-15 11:07:21 -0500

    feat: Creating the Admin's Delete Controller.

---
+++ Controllers/V1/Admins/AdminsDeleteController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assessment_BernersLee_API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment_BernersLee_API.Controllers.V1.Admins;
+public class AdminsDeleteController : AdminsController
+{
+    public AdminsDeleteController(IAdminRepository adminRepository) : base(adminRepository){}
+
+    //DELETE: /api/v1/categories/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            await _adminRepository.Delete(id);
+            return Ok("Admin deleted successfully!");
+        }
+        catch (ArgumentNullException)
+        {
+            return NotFound($"Admin with id {id} not found.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
