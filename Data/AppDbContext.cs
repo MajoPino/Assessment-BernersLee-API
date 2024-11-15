5821c4e126bbb5240cca7e746e197958f1fb1c6f
MajoPino <majo12pino@gmail.com>
2024-11-15 09:37:22 -0500

    feat: The seeders were uploaded to the database.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Assessment_BernersLee_API.Models;
+using Assessment_BernersLee_API.Seeders;
 using Microsoft.EntityFrameworkCore;
 
 namespace Assessment_BernersLee_API.Data;
@@ -10,4 +11,10 @@ public class AppDbContext : DbContext
 {
     public DbSet<Admin> Admins { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        AdminSeeder.Seed(modelBuilder);
+    }
 }
---
+++ Migrations/20241115143436_SeedAdmins.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Assessment_BernersLee_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdmins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "admins",
+                columns: new[] { "id", "email", "last_name", "name", "password" },
+                values: new object[,]
+                {
+                    { 1, "erwin.smith@example.com", "smith", "erwin", "password123" },
+                    { 2, "majopino@example.com", "pino", "majo", "password456" },
+                    { 3, "admin@example.com", "admin", "admin", "admin123" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "admins",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "admins",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "admins",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
