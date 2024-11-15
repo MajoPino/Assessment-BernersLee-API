44fc236a23e25931efc990343f8979ca3e130b3d
MajoPino <majo12pino@gmail.com>
2024-11-15 08:18:17 -0500

    feat: The AppDbContext file was created.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assessment_BernersLee_API.Data;
+public class AppDbContext : DbContext
+{
+
+}
