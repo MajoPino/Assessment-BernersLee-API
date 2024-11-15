0104f2b6de0d103fbcc39bf1a417deeb6790fa2b
MajoPino <majo12pino@gmail.com>
2024-11-15 11:34:17 -0500

    feat: The data Annotations of the Medic model were added.

--- Models/Medic.cs
+++ Models/Medic.cs
@@ -1,18 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Assessment_BernersLee_API.Models;
+
+[Table("medics")]
 public class Medic
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string LastName { get; set; }
-    public string IdentificationNumber { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
+    
+    [Required]
+    [Column("name")]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public required string Name { get; set; }
+
+    [Required]
+    [Column("last_name")]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public required string LastName { get; set; }
+
+    [Required]
+    [Column("identification_number")]
+    [MaxLength(50)]
+    [MinLength(4)]
+    public required string IdentificationNumber { get; set; }
+
+    [Required]
+    [Column("email")]
+    [MaxLength(150)]
+    [MinLength(6)]
+    public required string Email { get; set; }
+
+    [Required]
+    [Column("phone_number")]
+    [MaxLength(20)]
+    [MinLength(4)]
+    public required string PhoneNumber { get; set; }
+
+    [Required]
+    [Column("password")]
+    [MaxLength(100)]
+    [MinLength(8)]
+    public required string Password { get; set; }
 
     public Medic(){}
 
