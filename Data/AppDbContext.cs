8d7ff728d2e0905b0bd10f9b456a8fefb51c80bf
MajoPino <majo12pino@gmail.com>
2024-11-15 08:36:20 -0500

    feat: The model Admin was set on the AppDbContext file.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Assessment_BernersLee_API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Assessment_BernersLee_API.Data;
 public class AppDbContext : DbContext
 {
-
+    public DbSet<Admin> Admins { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
