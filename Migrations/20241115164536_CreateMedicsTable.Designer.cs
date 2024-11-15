ff2bb71daa4731790e2c4967736524ffb28c6e64
MajoPino <majo12pino@gmail.com>
2024-11-15 11:47:19 -0500

    feat: The Medic migrations were created & uploaded to the database.

--- Migrations/20241115164536_CreateMedicsTable.Designer.cs
+++ Migrations/20241115164536_CreateMedicsTable.Designer.cs
@@ -3,6 +3,7 @@ using Assessment_BernersLee_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Assessment_BernersLee_API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115164536_CreateMedicsTable")]
+    partial class CreateMedicsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
