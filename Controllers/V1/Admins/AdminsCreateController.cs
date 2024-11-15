a2c61ea345e3bd5364e62d83e5642098b89fd2fc
MajoPino <majo12pino@gmail.com>
2024-11-15 10:58:09 -0500

    feat: Adding some NuGets & creating the Admin's Create Controller.

---
+++ Controllers/V1/Admins/AdminsCreateController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assessment_BernersLee_API.DTOs;
+using Assessment_BernersLee_API.Models;
+using Assessment_BernersLee_API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment_BernersLee_API.Controllers.V1.Admins;
+public class AdminsCreateController : AdminsController
+{
+    public AdminsCreateController(IAdminRepository adminRepository) : base(adminRepository){}
+
+    //POST: /api/v1/categories
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] AdminDTO adminDTO)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var admin = new Admin
+        {
+            Id = adminDTO.Id,
+            Name = adminDTO.Name,
+            LastName = adminDTO.LastName,
+            Email = adminDTO.Email,
+            Password = adminDTO.Password
+            
+        };
+        try
+        {
+            await _adminRepository.Create(admin);
+            return Ok(admin);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
